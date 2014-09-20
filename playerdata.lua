75efe022c982305b8a026608b9948494e55fa14b
Izzette <aircowpeacock@gmail.com>
2014-09-20 11:07:32 -0500

    IMPORTANT: Changed Collisions
    Now, only \\ player.objects.ship // and \\ player.objects.shield; if shield.solid // actively check for collisions in their // .update \\ methods.  *All* objects are set for *passive collision* detection with the \\ collider //.  They no longer have \\ .onCollide // methods, and they call the other objects \\ .onCollide // method.  When objects collide with the shield, if they have \\ .typeN == "bullet" // then they will trigger a collision response in the shield.  When any shape collides with the ship, it's \\ .onCollide // method will not be called and it's \\ .hp // will be 0.   If you want collisions to be checked on \\ computer.objects // you can do that, but you still need to have a \\ .onCollide // method, even if it doesn't do anything, ie. \\ .onCollide = function(self, obj) end //.

--- playerdata.lua
+++ playerdata.lua
@@ -5,6 +5,13 @@ function refShip(var)
     ref.shape = collider:addPolygon(225, 475, 215, 505, 235, 505)  -- icosolis triangle, 30 high, 20 wide
     ref.update = function(self, dt)  -- called every frame, this one is just the on spawn function
       collider:addToGroup("player", self.shape)  -- add to player
+      collider:setPassive(self.shape) -- active collison detection is programed into this object in the update function
+      for i,v in ipairs(computer.objects) do  -- ^^ here it is ^^
+        if self:collidesWith(v.shape) then  -- called when collides with other shape, that shape's object is passed
+          v:onCollide(self) -- call other collision
+          v.hp = 0  -- set obj.hp to zero, will be removed when obj.update next called
+        end
+      end
       local ship = refShip("update[2]")  -- redefine ship.update
       self.update = ship
     end
@@ -29,13 +36,15 @@ function refShip(var)
     end
     ref.keypressed = function(self, key) end
     ref.keyreleased = function(self, key) end
-    ref.onCollide = function(self, obj) -- called when collides with other shape, that shape's object is passed
-      obj.hp = 0  -- set obj.hp to zero, will be removed when obj.update next called
-    end
   elseif var == "update[2]" then
     ref = function(self, dt) -- will be update function
+      for i,v in ipairs(computer.objects) do
+        if self:collidesWith(v.shape) then  -- active collison detection, that shape's object is passed
+          v:onCollide(self)  -- calls other objects collison method
+          v.hp = 0  -- set obj.hp to zero, will be removed when obj.update next called
+        end
+      end
       if self.hp <= 0 then  -- on death
-        collider:setPassive(self.shape)  -- stop collisions
         for i,v in ipairs(player.objects.shield.bullets) do
           -- fire randomly
         end
@@ -72,13 +81,37 @@ function refShield(var)
     ref.shape = collider:addCircle(225, 492, 25) -- circle with radius of 25
     ref.bullets = {} -- bullets directory
     ref.update = function(self, dt)  -- on spawn update function
-      if love.keyboard.isDown(" ") then  -- if the space bar is already down
-        self.solid = true -- set self.solid to true, as the shield is active
-      else
-        collider:setPassive(self.shape)  -- turn sheild collisons with other passive objects (most computer.objects) off, saves RAM
-        self.solid = false -- define shield off
-      end
+      collider:setPassive(self.shape)  -- turn off active collison checks with the collider
       collider:addToGroup("player", self.shape) -- add to group "player" ship and shield will not collide
+      if love.keyboard.isDown(" ") then
+        for i,v in ipairs(computer.objects) do  -- does not call v:
+          if self:collidesWith(v.shape) and v.typeN ~= nil and v.typeN == "bullet" and #self.bullets < 7 then  -- if the shield is on, the object has a name, that name is bullet, and the number of bullets is less than the capacity, seven
+            local bullet = {}  -- create local bullet
+            bullet.shape = v.shape  -- copy shape
+            bullet.x, bullet.y = v.shape:center()  -- define position
+            bullet.velocity.x = ((math.random() - 0.5) * 25) + ((ship.x - self.x) * 3)  -- random, tends towards ship
+            bullet.velocity.y = ((math.random() - 0.5) * 25) + ((ship.y - self.y) * 3)
+            bullet.move = function(self, dt)  -- independant move function
+              self.x, self.y = self.shape:center()  -- find position
+              if math.sqrt(((self.x - ship.x) ^ (2)) + ((self.y - ship.y) ^ 2)) > 25 then -- if far from ship
+                self.velocity.x = ((math.random() - 0.5) * 25) + ((ship.x - self.x) * 3)  -- tend velocity twoards ship
+                self.velocity.y = ((math.random() - 0.5) * 25) + ((ship.y - self.y) * 3)
+              end
+              self.shape:move(self.velocity.x * dt, self.velocity.y * dt)   -- move based on velocity
+            end
+            bullet.draw = function(self) -- draw bullet
+              love.graphics.setColor(255, 255, 255, 150)  -- ghostly white
+              self.shape:draw("fill")
+              love.graphics.setColor(0, 255, 255, 100)  -- cyan outline
+              self.shape:draw("line")
+            end
+            table.insert(self.bullets, bullet)  -- add bullet
+            collider:setPassive(self.bullets[#self.bullets].shape) -- no need to actively check for collisions
+            collider:addToGroup("player", self.bullets[#self.bullets].shape)  -- prevent collisions
+          end
+          v.hp = 0  -- hp to zero, will be deleted in next v.update call
+        end
+      end
       local shield = refShield("update[2]")  -- redefine update
       self.update = shield
     end
@@ -122,52 +155,40 @@ function refShield(var)
         v:draw()
       end
     end
-    ref.keypressed = function(self, key)  -- called when a key is pressed
-      if key == " " and not self.solid then -- if the space is pressed and it's not solid already
-        collider:setActive(self.shape)  -- check active for collisions
-        self.solid = true  -- now true
-      elseif key == "j" and #self.bullets > 0 then  -- for bullet fire
-        -- create bullet
-        table.remove(self.bullets, 1)
-      end
-    end
-    ref.keyreleased = function(self, key)  -- called when a key is released
-      if key == " " and self.solid then  -- if it was space and it is solid
-        collider:setPassive(self.shape)  -- set passive collison detection
-        self.solid = false  -- not solid
-      end
-    end
-    ref.onCollide = function(self, obj) -- called when objects collide
-      if typeN ~= nil and typeN == "bullet" and #self.bullets < 7 and self.solid then  -- if the shield is on, the object has a name, that name is bullet, and the number of bullets is less than the capacity, seven
-        local bullet = {}  -- create local bullet
-        bullet.shape = obj.shape  -- copy shape
-        bullet.x, bullet.y = obj.shape:center()  -- define position
-        bullet.velocity.x = ((math.random() - 0.5) * 25) + ((ship.x - self.x) * 3)  -- random, tends towards ship
-        bullet.velocity.y = ((math.random() - 0.5) * 25) + ((ship.y - self.y) * 3)
-        bullet.move = function(self, dt)  -- independant move function
-          self.x, self.y = self.shape:center()  -- find position
-          if math.sqrt(((self.x - ship.x) ^ (2)) + ((self.y - ship.y) ^ 2)) > 25 then -- if far from ship
-            self.velocity.x = ((math.random() - 0.5) * 25) + ((ship.x - self.x) * 3)  -- tend velocity twoards ship
-            self.velocity.y = ((math.random() - 0.5) * 25) + ((ship.y - self.y) * 3)
-          end
-          self.shape:move(self.velocity.x * dt, self.velocity.y * dt)   -- move based on velocity
-        end
-        bullet.draw = function(self) -- draw bullet
-          love.graphics.setColor(255, 255, 255, 150)  -- ghostly white
-          self.shape:draw("fill")
-          love.graphics.setColor(0, 255, 255, 100)  -- cyan outline
-          self.shape:draw("line")
-        end
-        table.insert(self.bullets, bullet)  -- add bullet
-        collider:setPassive(self.bullets[#self.bullets].shape) -- no need to actively check for collisions
-        collider:addToGroup("player", self.bullets[#self.bullets].shape)  -- prevent collisions
-      end
-      obj.hp = 0  -- hp to zero, will be deleted in next obj.update call
-    end
+    
   elseif var == "update[2]" then  -- second update phase, main phase
     ref = function(self, dt)
-      if love.keyboard.isDown(" ") then  -- if space is down
-        self.hp = self.hp - (40 * dt)  -- drain charge
+      if love.keyboard.isDown(" ") and (self.hp - (40 * dt)) >= 0 then
+        self.hp = self.hp - (40 * dt)
+        for i,v in ipairs(computer.objects) do  -- does not call v:
+          if self:collidesWith(v.shape) and v.typeN ~= nil and v.typeN == "bullet" and #self.bullets < 7 then  -- if the shield is on, the object has a name, that name is bullet, and the number of bullets is less than the capacity, seven
+            local bullet = {}  -- create local bullet
+            bullet.shape = v.shape  -- copy shape
+            bullet.x, bullet.y = v.shape:center()  -- define position
+            bullet.velocity.x = ((math.random() - 0.5) * 25) + ((ship.x - self.x) * 3)  -- random, tends towards ship
+            bullet.velocity.y = ((math.random() - 0.5) * 25) + ((ship.y - self.y) * 3)
+            bullet.move = function(self, dt)  -- independant move function
+              self.x, self.y = self.shape:center()  -- find position
+              if math.sqrt(((self.x - ship.x) ^ (2)) + ((self.y - ship.y) ^ 2)) > 25 then -- if far from ship
+                self.velocity.x = ((math.random() - 0.5) * 25) + ((ship.x - self.x) * 3)  -- tend velocity twoards ship
+                self.velocity.y = ((math.random() - 0.5) * 25) + ((ship.y - self.y) * 3)
+              end
+              self.shape:move(self.velocity.x * dt, self.velocity.y * dt)   -- move based on velocity
+            end
+            bullet.draw = function(self) -- draw bullet
+              love.graphics.setColor(255, 255, 255, 150)  -- ghostly white
+              self.shape:draw("fill")
+              love.graphics.setColor(0, 255, 255, 100)  -- cyan outline
+              self.shape:draw("line")
+            end
+            table.insert(self.bullets, bullet)  -- add bullet
+            collider:setPassive(self.bullets[#self.bullets].shape) -- no need to actively check for collisions
+            collider:addToGroup("player", self.bullets[#self.bullets].shape)  -- prevent collisions
+          end
+          v.hp = 0  -- hp to zero, will be deleted in next v.update call
+        end
+      elseif love.keyboard.isDown(" ") and (self.hp - (40 * dt)) < 0 then
+        self.hp = 0
       elseif self.hp <= (100 - (15 * dt)) then  -- if not and not within grounds of going over 100
         self.hp = self.hp + (15 * dt)  -- recharge slowly
       else  -- otherwise
@@ -188,9 +209,34 @@ function refShield(var)
     ref = function(self, dt)
       self.hp = self.hp + (15 * dt)  -- recharge slowly
       if self.hp >= 50 then  -- when halfway
-        if love.keyboard.isDown(" ") and not self.solid then  -- check to see if shield should be solid
-          collider:setActive(self.shape)  -- make active
-          self.solid = true  -- set solid
+        if love.keyboard.isDown(" ") then
+          for i,v in ipairs(computer.objects) do  -- does not call v:
+            if self:collidesWith(v.shape) and v.typeN ~= nil and v.typeN == "bullet" and #self.bullets < 7 then  -- if the shield is on, the object has a name, that name is bullet, and the number of bullets is less than the capacity, seven
+              local bullet = {}  -- create local bullet
+              bullet.shape = v.shape  -- copy shape
+              bullet.x, bullet.y = v.shape:center()  -- define position
+              bullet.velocity.x = ((math.random() - 0.5) * 25) + ((ship.x - self.x) * 3)  -- random, tends towards ship
+              bullet.velocity.y = ((math.random() - 0.5) * 25) + ((ship.y - self.y) * 3)
+              bullet.move = function(self, dt)  -- independant move function
+                self.x, self.y = self.shape:center()  -- find position
+                if math.sqrt(((self.x - ship.x) ^ (2)) + ((self.y - ship.y) ^ 2)) > 25 then -- if far from ship
+                  self.velocity.x = ((math.random() - 0.5) * 25) + ((ship.x - self.x) * 3)  -- tend velocity twoards ship
+                  self.velocity.y = ((math.random() - 0.5) * 25) + ((ship.y - self.y) * 3)
+                end
+                self.shape:move(self.velocity.x * dt, self.velocity.y * dt)   -- move based on velocity
+              end
+              bullet.draw = function(self) -- draw bullet
+                love.graphics.setColor(255, 255, 255, 150)  -- ghostly white
+                self.shape:draw("fill")
+                love.graphics.setColor(0, 255, 255, 100)  -- cyan outline
+                self.shape:draw("line")
+              end
+              table.insert(self.bullets, bullet)  -- add bullet
+              collider:setPassive(self.bullets[#self.bullets].shape) -- no need to actively check for collisions
+              collider:addToGroup("player", self.bullets[#self.bullets].shape)  -- prevent collisions
+            end
+            v.hp = 0  -- hp to zero, will be deleted in next v.update call
+          end
         end
         local shield = refShield()  -- call sheild data
         self.draw = shield.draw  -- restart draw
