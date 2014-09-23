375f7d84337f375b0f2fc89c8196e0e1ed7e6b49
Izzette <aircowpeacock@gmail.com>
2014-09-23 15:10:16 -0500

    started graphics library

--- graphics.lua
+++ graphics.lua
@@ -17,12 +17,16 @@ function loadGraphics()  -- load graphics
       if str == "mainmenu" then
         self.objects = {}
         local mainmenu = {}
+        mainmenu.fontAbductionMed = love.graphics.newFont("abduction/Abduction.ttf", 14)
+        mainmenu.fontAbductionBig = love.graphics.newFont("abduction/Abduction.ttf", 18)
         mainmenu.objects = {}
         mainmenu.draw = function(self, x, y)
           love.graphics.setColor(0, 0, 0)
           love.graphics.rectangle("fill", 0, 0, 450, 550)
+          love.graphics.setFont(self.fontAbductionBig)
           love.graphics.setColor(0, 255, 0)
-          love.graphics.printf("Rebound Shooter: Resurgence", 25, 100, 400, "center", 0, 1.5, 1.5)
+          love.graphics.printf("Rebound Shooter: Resurgence", 25, 100, 400, "center")
+          love.graphics.setFont(self.fontAbductionMed)
           for i,v in ipairs(self.objects) do
             v:draw(x, y)
           end
@@ -39,17 +43,17 @@ function loadGraphics()  -- load graphics
           loadPlayer()
           graphics:setGraphics("game")
         end
-        obj = self:addButton("Play", 100, 250, 3, 250, 0, 0, 255, 200, action)
+        obj = self:addButton("Play", 100, 250, 1, 250, 0, 0, 255, 200, action)
         table.insert(mainmenu.objects, obj)
         action = function(self)
           -- graphics:setGraphics("credits")
         end
-        obj = self:addButton("Credits", 100, 325, 3, 250, 0, 0, 255, 200, action)
+        obj = self:addButton("Credits", 100, 325, 1, 250, 0, 0, 255, 200, action)
         table.insert(mainmenu.objects, obj)
         action = function(self)
           love.event.quit()
         end
-        obj = self:addButton("Exit Game", 100, 400, 3, 250, 0, 0, 255, 200, action)
+        obj = self:addButton("Exit Game", 100, 400, 1, 250, 0, 0, 255, 200, action)
         table.insert(mainmenu.objects, obj)
         table.insert(self.objects, mainmenu)
       elseif str == "game" then
@@ -67,12 +71,16 @@ function loadGraphics()  -- load graphics
       elseif str == "pause" then
         graphics:setGraphics("game")
         local pause = {}
+        pause.fontAbductionSm = love.graphics.newFont("abduction/Abduction.ttf", 12)
+        pause.fontAbductionMed = love.graphics.newFont("abduction/Abduction.ttf", 14)
         pause.objects = {}
         pause.draw = function(self, x, y)
           love.graphics.setColor(0, 0, 0, 200)
           love.graphics.rectangle("fill", 100, 125, 250, 300)
           love.graphics.setColor(255, 255, 255, 255)
-          love.graphics.printf("Pause", 125, 175, 200, "center", 0, 1.25, 1.25)
+          love.graphics.setFont(self.fontAbductionMed)
+          love.graphics.printf("Pause", 125, 175, 200, "center")
+          love.graphics.setFont(self.fontAbductionSm)
           for i,v in ipairs(self.objects) do
             v:draw(x, y)
           end
@@ -86,28 +94,32 @@ function loadGraphics()  -- load graphics
           play = true
           graphics:setGraphics("game")
         end
-        local obj = self:addButton("Resume", 125, 250, 2, 200, 0, 0, 255, 200, action)
+        local obj = self:addButton("Resume", 125, 250, 1, 200, 0, 0, 255, 200, action)
         table.insert(pause.objects, obj)
         action = function(self)
           graphics:setGraphics("toMain")
         end
-        obj = self:addButton("Mainmenu", 125, 300, 2, 200, 0, 0, 255, 200, action)
+        obj = self:addButton("Mainmenu", 125, 300, 1, 200, 0, 0, 255, 200, action)
         table.insert(pause.objects, obj)
         action = function(self)
           graphics:setGraphics("toQuit")
         end
-        obj = self:addButton("Exit Game", 125, 350, 2, 200, 0, 0, 255, 200, action)
+        obj = self:addButton("Exit Game", 125, 350, 1, 200, 0, 0, 255, 200, action)
         table.insert(pause.objects, obj)
         table.insert(self.objects, pause)
       elseif str == "toMain" then
         graphics:setGraphics("game")
         local toMain = {}
+        toMain.fontAbductionMed = love.graphics.newFont("abduction/Abduction.ttf", 14)
+        toMain.fontAbductionSm = love.graphics.newFont("abduction/Abduction.ttf", 12)
         toMain.objects = {}
         toMain.draw = function(self, x, y)
           love.graphics.setColor(0, 0, 0, 200)
           love.graphics.rectangle("fill", 100, 125, 200, 300)
           love.graphics.setColor(0, 255, 0)
-          love.graphics.printf("Progress will be Lost", 125, 200, 200, "center", 0, 1.25, 1.25)
+          love.graphics.setFont(self.fontAbductionMed)
+          love.graphics.printf("Progress will be Lost", 125, 200, 200, "center")
+          love.graphics.setFont(self.fontAbductionSm)
           for i,v in ipairs(self.objects) do
             v:draw(x, y)
           end
@@ -120,25 +132,29 @@ function loadGraphics()  -- load graphics
         local action = function(self)
           graphics:setGraphics("pause")
         end
-        local obj = self:addButton("Cancel", 125, 375, 2, 85, 255, 255, 255, 200, action)
+        local obj = self:addButton("Cancel", 125, 375, 1, 85, 255, 255, 255, 200, action)
         table.insert(toMain.objects, obj)
         action = function(self)
           player.objects = {}
           computer.objects = {}
           graphics:setGraphics("mainmenu")
         end
-        obj = self:addButton("Ok", 240, 375, 2, 85, 255, 0, 0, 200, action)
+        obj = self:addButton("Ok", 240, 375, 1, 85, 255, 0, 0, 200, action)
         table.insert(toMain.objects, obj)
         table.insert(graphics.objects, toMain)
       elseif str == "toQuit" then
         graphics:setGraphics("game")
         local toQuit = {}
+        toQuit.fontAbductionMed = love.graphics.newFont("abduction/Abduction.ttf", 14)
+        toQuit.fontAbductionSm = love.graphics.newFont("abduction/Abduction.ttf", 12)
         toQuit.objects = {}
         toQuit.draw = function(self, x, y)
           love.graphics.setColor(0, 0, 0, 200)
           love.graphics.rectangle("fill", 125, 125, 200, 300)
           love.graphics.setColor(0, 255, 0)
-          love.graphics.printf("Progress will be Lost", 125, 200, 200, "center", 0, 1.25, 1.25)
+          love.graphics.setFont(self.fontAbductionMed)
+          love.graphics.printf("Progress will be Lost", 125, 200, 200, "center")
+          love.graphics.setFont(self.fontAbductionSm)
           for i,v in ipairs(self.objects) do
             v:draw(x, y)
           end
@@ -151,12 +167,12 @@ function loadGraphics()  -- load graphics
         local action = function(self)
           graphics:setGraphics("pause")
         end
-        local obj = self:addButton("Cancel", 125, 375, 2, 85, 255, 255, 255, 200, action)
+        local obj = self:addButton("Cancel", 125, 375, 1, 85, 255, 255, 255, 200, action)
         table.insert(toQuit.objects, obj)
         action = function(self)
           love.event.quit()
         end
-        obj = self:addButton("Ok", 240, 375, 2, 85, 255, 0, 0, 200, action)
+        obj = self:addButton("Ok", 240, 375, 1, 85, 255, 0, 0, 200, action)
         table.insert(toQuit.objects, obj)
         table.insert(graphics.objects, toQuit)
       end
@@ -181,7 +197,7 @@ function loadGraphics()  -- load graphics
       else
         love.graphics.setColor(self.red, self.green, self.blue, self.opacity)
       end
-      love.graphics.printf(self.string, self.x, self.y, self.width / ((0.125 * self.size) + 0.875), "center", 0, ((0.125 * self.size) + 0.875), ((0.125 * self.size) + 0.875))
+      love.graphics.printf(self.string, self.x, self.y - 8, self.width / ((0.125 * self.size) + 0.875), "center")
     end
     obj.mousepressed = function(self, x, y, key)
       if x > self.x and x < self.x + self.width and y > self.y and y < self.y + (20 * self.size) and key == "l" then
